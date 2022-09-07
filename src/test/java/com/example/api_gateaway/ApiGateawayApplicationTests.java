d6357eb4fc2a57525817588db93b02a81edb1938
NomadKim <99548558+NomadKim@users.noreply.github.com>
2022-09-07 22:48:03 +0900

    Initial commit

---
+++ src/test/java/com/example/api_gateaway/ApiGateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_gateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
